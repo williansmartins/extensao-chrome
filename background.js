083308bf15da408a3a61e155e7b893b61aad508c
willians martins <professorwillians@gmail.com>
2016-09-21 13:50:41 -0300

    incremento de logica

--- background.js
+++ background.js
@@ -6,14 +6,39 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
+// var cpf = "102.209.277-40";
+// var cpf = "010.216.908-05";
+var cpf = "010.323.958-85";
+
+// var senha="95.41.57";
+// var senha="57.72.74";
+var senha="37.68.36";
+
 chrome.pageAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, {
-    	code: 
-    	"(document.evaluate(\"//label[contains(text(),'5')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" + 
-    	"(document.evaluate(\"//label[contains(text(),'5')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" + 
-    	"(document.evaluate(\"//label[contains(text(),'5')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" + 
-    	"(document.evaluate(\"//label[contains(text(),'5')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" + 
-    	"(document.evaluate(\"//label[contains(text(),'5')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" + 
-    	"(document.evaluate(\"//label[contains(text(),'5')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();"
-    });
+    chrome.tabs.executeScript(
+        tab.id, 
+        // {
+        //  code:  
+        //   "(document.evaluate(\"//label[contains(text(),'4')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+        //   "(document.evaluate(\"//label[contains(text(),'0')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+        //   "(document.evaluate(\"//label[contains(text(),'1')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+        //   "(document.evaluate(\"//label[contains(text(),'8')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+        //   "(document.evaluate(\"//label[contains(text(),'9')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+        //   "(document.evaluate(\"//label[contains(text(),'5')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+        //   "(document.evaluate(\"//input[@id='loggin']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();"
+        // },        
+
+        {
+         code:  
+          "document.getElementById(\"cpf\").value = \""+cpf+"\";" +
+          "(document.evaluate(\"//label[contains(text(),'"+senha[0]+"')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+          "(document.evaluate(\"//label[contains(text(),'"+senha[1]+"')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+          "(document.evaluate(\"//label[contains(text(),'"+senha[3]+"')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+          "(document.evaluate(\"//label[contains(text(),'"+senha[4]+"')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+          "(document.evaluate(\"//label[contains(text(),'"+senha[6]+"')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+          "(document.evaluate(\"//label[contains(text(),'"+senha[7]+"')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
+          "(document.evaluate(\"//input[@id='loggin']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();"
+        },
+        function(results){ console.log(results); }
+    );
 });
