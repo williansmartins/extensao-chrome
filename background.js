671f841c6083e9c784da0090a5a8c66b3b50388e
willians martins <professorwillians@gmail.com>
2016-11-25 11:15:58 -0200

    limpando minha maquina

--- background.js
+++ background.js
@@ -8,26 +8,17 @@ chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
 // var cpf = "102.209.277-40";
 // var cpf = "010.216.908-05";
-var cpf = "010.323.958-85";
+// var cpf = "010.323.958-85";
+var cpf = "056.801.489-86";
 
 // var senha="95.41.57";
 // var senha="57.72.74";
-var senha="37.68.36";
+// var senha="37.68.36";
+var senha="65.43.21";
 
 chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(
         tab.id, 
-        // {
-        //  code:  
-        //   "(document.evaluate(\"//label[contains(text(),'4')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
-        //   "(document.evaluate(\"//label[contains(text(),'0')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
-        //   "(document.evaluate(\"//label[contains(text(),'1')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
-        //   "(document.evaluate(\"//label[contains(text(),'8')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
-        //   "(document.evaluate(\"//label[contains(text(),'9')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
-        //   "(document.evaluate(\"//label[contains(text(),'5')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();" +
-        //   "(document.evaluate(\"//input[@id='loggin']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).click();"
-        // },        
-
         {
          code:  
           "document.getElementById(\"cpf\").value = \""+cpf+"\";" +
